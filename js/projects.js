3141e8fa52b5a7547c36b57b0bd817db67fda532
Scott Weaver <sweaver69@gmail.com>
2016-05-03 21:25:11 -0700

    New upload
    Forked from Yelp

---
+++ js/projects.js
@@ -0,0 +1,21 @@
+oss_projects = {};
+oss_projects['meteor-blast'] = {
+  featured: true,
+  position: 4,
+  background: 'meteorblast.jpg'
+}
+oss_projects['ScottWeaverSite'] = {
+  featured: true,
+  position: 3,
+  background: 'scottweaversite.jpg'
+};
+oss_projects['SemesterScheduleApp'] = {
+  featured: true,
+  position: 1,
+  background: 'cheetah.jpg'
+};
+oss_projects['StudentEnrollmentManagement'] = {
+  featured: true,
+  position: 2,
+  background: 'javastudents.jpg'
+}
